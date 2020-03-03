787b4ecd01e73bad55a6de83dee0b944fb6eb97e
Tobias Jonas <t.jonas@innfactory.de>
2020-03-03 18:21:52 +0100

    ci

---
+++ .circleci/test.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Testing with datastore emulator"
+echo "Starting database..."
+gcloud beta emulators datastore start --no-store-on-disk --consistency=1.0 &
+sleep 5
+curl -X POST http://localhost:8081/reset
+cd ..
+sbt test
+cd .circleci
+trap 'kill $(jobs -pr)' SIGINT SIGTERM EXIT
